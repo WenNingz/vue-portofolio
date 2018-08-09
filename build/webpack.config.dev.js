e16f18d43d5a87d26f708cfa5cc2f54c823c5bbd
Ning <wenning.me@gmail.com>
2018-08-09 11:43:37 +0900

    Setup: Add Vue eslint to detect issues or enforce style guideline
    - Installed NPM packages
    - Created .eslintrc.js to define rules
    - Added NPM script to run ESLint

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -43,6 +43,11 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.(js|vue)$/,
+        use: 'eslint-loader',
+        enforce: 'pre'
       }
     ]
   },
